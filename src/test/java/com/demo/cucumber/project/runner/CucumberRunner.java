c900a7c8713b6cfa23d0df1298e1a82de5de930e
Arkadiusz <karas.tsukasa@gmail.com>
2019-12-21 21:47:05 +0100

    fix for feature and steps paths

--- src/test/java/com/demo/cucumber/project/runner/CucumberRunner.java
+++ src/test/java/com/demo/cucumber/project/runner/CucumberRunner.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/wordpresstests/featureFiles"},
-        glue = "wordpresstests.steps",
+@CucumberOptions(features = {"src/test/resources/features/"},
+        glue = "com.demo.cucumber.project.steps",
         monochrome = true,
         plugin = {"pretty", "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"})
 public class CucumberRunner extends AbstractTestNGCucumberTests {

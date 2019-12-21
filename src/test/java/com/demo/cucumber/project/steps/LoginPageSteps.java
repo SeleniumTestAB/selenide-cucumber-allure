ea9ee975ae186665b408145da2c356afb0f5038b
Arkadiusz <karas.tsukasa@gmail.com>
2019-12-21 22:40:03 +0100

    add screenshot to cucumber

--- src/test/java/com/demo/cucumber/project/steps/LoginPageSteps.java
+++ src/test/java/com/demo/cucumber/project/steps/LoginPageSteps.java
@@ -2,26 +2,30 @@ package com.demo.cucumber.project.steps;
 
 import com.demo.cucumber.project.config.PropertyFileReader;
 import com.demo.cucumber.project.pages.LoginPage;
+import io.cucumber.core.api.Scenario;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class LoginPageSteps {
     private LoginPage loginPage = new LoginPage();
     private String adminLogin, adminPassword;
-
+    private Scenario scenario;
    @Before
-    public void getLoginAndPassword() {
+    public void getLoginAndPassword(Scenario scenario) {
         adminLogin = PropertyFileReader.getProperty("setup.properties", "adminLogin");
         adminPassword = PropertyFileReader.getProperty("setup.properties", "adminPassword");
+        this.scenario = scenario;
 
     }
     @Given("User is on Login Page")
     public void user_is_on_Login_Page() {
         open(loginPage.getLoginPageUrl());
+        scenario.embed(screenshot("loginPage").getBytes(), "image/png");
     }
 
     @When("User gives proper credentials to Admin account")
